1b88df25f46d97e0641c54b5af9b3e01a532e53c
Trung <trungtranduc19@gmail.com>
2020-03-02 22:42:47 +0700

    update

---
+++ MSS_DEMO/Core/Implement/CoursesDeadlineRepository.cs
@@ -0,0 +1,17 @@
+﻿using MSS_DEMO.Models;
+using MSS_DEMO.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSS_DEMO.Core.Implement
+{
+    public class CoursesDeadlineRepository : GenericRepository<Course_Deadline>
+    {
+        public CoursesDeadlineRepository(MSSEntities context)
+           : base(context)
+        {
+        }
+    }
+}
