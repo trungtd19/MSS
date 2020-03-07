32b78bcef617f6a91a0acd5afa4b26583affecca
Trung <trungtranduc19@gmail.com>
2020-03-07 19:27:03 +0700

    update filter

---
+++ MSS_DEMO/Models/CoursesReportViewModel.cs
@@ -0,0 +1,17 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSS_DEMO.Models
+{
+    public class CoursesReportViewModel : Student_Course_Log
+    {
+        public IPagedList<Student_Course_Log> PageList;
+        public string searchCheck { get; set; }
+        public string option { get; set; }
+        public IList<string> listOption;
+        public IList<string> listSubject;
+    }
+}
