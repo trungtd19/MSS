81ee8c4cc79a896e8c307288700db1a105b0aedf
Trung <trungtranduc19@gmail.com>
2020-04-20 22:35:59 +0700

    update function

--- MSS_DEMO/Core/Implement/StudentSpecificationLogRepository.cs
+++ MSS_DEMO/Core/Implement/StudentSpecificationLogRepository.cs
@@ -2,6 +2,7 @@
 using MSS_DEMO.Repository;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 
@@ -26,6 +27,30 @@ namespace MSS_DEMO.Core.Implement
                 return student;
             }
 
+        }
+        public int CleanSpecificationReport(string ImportedDate, string Semester_ID)
+        {
+            int rowDelete = 0;
+            string sqlQuery = "";
+            if (!string.IsNullOrEmpty(ImportedDate) && !string.IsNullOrEmpty(Semester_ID))
+            {
+                DateTime dt = DateTime.ParseExact(ImportedDate, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                sqlQuery = "DELETE FROM Student_Specification_Log WHERE Date_Import = '" + dt + "' and Semester_ID = '" + Semester_ID + "'";
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(ImportedDate))
+                {
+                    DateTime dt = DateTime.ParseExact(ImportedDate, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                    sqlQuery = "DELETE FROM Student_Specification_Log WHERE Date_Import = '" + dt + "'";
+                }
+                else
+                {
+                    sqlQuery = "DELETE FROM Student_Specification_Log WHERE Semester_ID = '" + Semester_ID + "'";
+                }
+            }
+            rowDelete = context.Database.ExecuteSqlCommand(sqlQuery);
+            return rowDelete;
         }
     }
 }
