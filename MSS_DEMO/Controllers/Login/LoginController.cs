a21761c33b2493557d450e818de41daaf7f72c52
Trung <trungtranduc19@gmail.com>
2020-05-11 16:01:20 +0700

    Update LoginController.cs

--- MSS_DEMO/Controllers/Login/LoginController.cs
+++ MSS_DEMO/Controllers/Login/LoginController.cs
@@ -4,6 +4,7 @@ using MSS_DEMO.Models;
 using MSS_DEMO.MssService;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -25,48 +26,19 @@ namespace MSS_DEMO.Controllers.Login
 
             return View();
         }
-        //[HttpPost]
-        //public ActionResult Login(string UserName, string PassWord)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        UserName = UserName + "@fpt.edu.vn";
-        //        var user = db.User_Role.SingleOrDefault(x => x.Login.Equals(UserName));
-        //        if (user == null)
-        //        {
-        //            ModelState.AddModelError("", " Accont is not exist");
-        //        }
-        //        else if (user.isActive == false)
-        //        {
-        //            ModelState.AddModelError("", " Accont is locked ");
-        //        }
-        //        else
-        //        {
-        //            var role = user.Role_ID;
-        //            var UserSession = new UserLogin();
-        //            UserSession.UserID = user.User_ID;
-        //            UserSession.UserName = user.Login;
-        //            Session.Add(CommonConstants.ROLE_Session, role);
-        //            Session.Add(CommonConstants.User_Session, UserSession);
-        //            return RedirectToAction("Index", "Home");
-        //        }
-        //    }
-
-        //    else
-        //    {
-        //        ModelState.AddModelError("", "Login Fail");
-        //    }
-        //    return View("Login");
-
-        //}
         [HttpPost]
         public ActionResult LoginWithGoogle()
         {
             User_Role user = null;
             string userLogin = Request["Mail"];
             string[] temp = userLogin.Split('@');
-            string checkmail = temp[1];                    
-                user = db.User_Role.SingleOrDefault(x => x.Login.Equals(userLogin));
+            string checkmail = temp[1];
+            using (var ctx = new MSSEntities())
+            {
+                 user = ctx.User_Role
+                                .SqlQuery("Select * from User_Role where Login=@Login", new SqlParameter("@Login", userLogin))
+                                .FirstOrDefault();
+            }
             if (user != null)
             {
                 var RoleSession = new RoleLogin();
