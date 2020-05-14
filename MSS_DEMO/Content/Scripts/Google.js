7957efd88fbf2673d789bd5bf82078e5bf677c1f
datntse04909 <59784506+datntse04909@users.noreply.github.com>
2020-05-14 16:16:24 +0700

    update login

--- MSS_DEMO/Content/Scripts/Google.js
+++ MSS_DEMO/Content/Scripts/Google.js
@@ -1,30 +1,31 @@
-﻿function onSignIn(googleUser) {
-    // Useful data for your client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    //document.getElementById('profileinfo').innerHTML = profile.getEmail();
-    var Mail = profile.getEmail();
-    var formData = new FormData();
-    formData.append("Mail", Mail);
-    $.ajax({
-        url: '/Login/LoginWithGoogle',
-        type: 'POST',
-        data: formData,
-        dataType: 'json',
-        contentType: false,
-        processData: false,
-        success: function (data) {
-            if (data.message == "true") {
-                window.location.href = "/Home/Index"
-            }
-            if (data.message == "false") {
-                signOut();
-                sweetAlert("Error!","Login fail!", "error");
-            }
+﻿//function onSignIn(googleUser) {
+//    // Useful data for your client-side scripts:
+//    var profile = googleUser.getBasicProfile();
+//    //document.getElementById('profileinfo').innerHTML = profile.getEmail();
+//    var Mail = profile.getEmail();
+//    var formData = new FormData();
+//    var rootUrl = '@Url.Content("~")';
+//    formData.append("Mail", Mail);
+//    $.ajax({
+//        url: 'Login, LoginWithGoogle',
+//        type: 'POST',
+//        data: formData,
+//        dataType: 'json',
+//        contentType: false,
+//        processData: false,
+//        success: function (data) {
+//            if (data.message == "true") {
+//                window.location.href = "/Home/Index"
+//            }
+//            if (data.message == "false") {
+//                signOut();
+//                sweetAlert("Error!","Login fail!", "error");
+//            }
 
-        }
-    }).done(function () {
-    });
-}
+//        }
+//    }).done(function () {
+//    });
+//}
 
     function onLoad() {
      gapi.load('auth2', function () {
